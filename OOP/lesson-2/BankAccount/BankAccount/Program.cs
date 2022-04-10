b26509ddddbd26947373e49ba2df1caf9aaf5acf
Aphilome <1.katrin.1@mail.ru>
2022-04-10 15:43:13 +0300

    OOP

---
+++ OOP/lesson-2/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankAccount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var nikolayAccount = new BankAccount(100);
+			var kateAccount = new BankAccount(0, BankAccountTypeEnum.CorrespondentAccount);
+
+			nikolayAccount.ToDeposit(1);
+
+
+			Console.WriteLine(
+				$"Account number: {nikolayAccount.AccountNumber}, " +
+				$"balance: {nikolayAccount.Balance}, " +
+				$"account type: {nikolayAccount.BankAccountType}");
+			
+			Console.WriteLine(
+							$"Account number: {kateAccount.AccountNumber}, " +
+							$"balance: {kateAccount.Balance}, " +
+							$"account type: {kateAccount.BankAccountType}");
+		}
+	}
+}
---
+++ OOP/lesson-3/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankAccount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var nikolayAccount = new BankAccount(100);
+			var kateAccount = new BankAccount(0, BankAccountTypeEnum.CorrespondentAccount);
+
+			nikolayAccount.ToDeposit(1);
+			
+			Console.WriteLine(nikolayAccount);
+			Console.WriteLine(kateAccount);
+
+			kateAccount.MoneyTransactionToDeposit(nikolayAccount, 101);
+
+			Console.WriteLine(nikolayAccount);
+			Console.WriteLine(kateAccount);
+		}
+	}
+}
