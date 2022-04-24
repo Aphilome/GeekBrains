a79d5864fcdde223c3b44b1aecc6ff4c21d96298
Aphilome <1.katrin.1@mail.ru>
2022-04-24 18:57:28 +0300

    Controller preparation

---
+++ AsyncProgramming/Timesheets/Timesheets/Controllers/ContractController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timesheets.Data.Dto;
+
+namespace Timesheets.Controllers
+{
+    [Route("api/contracts")]
+    public class ContractController : Controller
+    {
+        [HttpPost("create")]
+        public IActionResult Create()
+        {
+            
+            return null;
+        }
+
+        [HttpGet("get/{id}")]
+        public IActionResult Get(long id)
+        {
+
+        }
+
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+
+        }
+
+        [HttpPut("update/{id}")]
+        public IActionResult Update(long id, [FromBody]ContractDto contractDto)
+        {
+
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult Remove(long id)
+        {
+
+        }
+    }
+}
---
+++ AsyncProgramming/Timesheets/Timesheets/Data/Dto/ContractDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Timesheets.Data.Dto
+{
+    public class ContractDto
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Номер договора
+        /// </summary>
+        public ulong Number { get; set; }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Дата подписания
+        /// </summary>
+        public DateTime SignDate { get; set; }
+
+        /// <summary>
+        /// Статус
+        /// </summary>
+        public ContractStatusEnum Status { get; set; }
+    }
+}
