75e9155cb3cb7b4714efcca19fffeab41e63be69
Aphilome <1.katrin.1@mail.ru>
2022-05-09 00:05:21 +0300

    my DbContext

---
+++ AsyncProgramming/Timesheets/Timesheets/Data/TimesheetsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Timesheets.Data.Entities;
+
+namespace Timesheets.Data
+{
+    public class TimesheetsDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Contract> Contracts { get; set; }
+        
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Invoice> Invoices { get; set; }
+
+        public DbSet<JobTask> JobTasks { get; set; }
+    }
+}
