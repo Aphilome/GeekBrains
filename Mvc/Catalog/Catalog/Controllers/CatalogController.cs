244324355bc475b49b74f6f79d7f7882d73e0d7e
Aura Philomena <1.katrin.1@mail.ru>
2022-06-13 21:28:30 +0300

    Products

---
+++ Mvc/Catalog/Catalog/Controllers/CatalogController.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private static Models.Catalog _catalog = new();
+
+        [HttpGet]
+        public IActionResult Categories()
+        {
+            return View(_catalog);
+        }
+
+        [HttpPost]
+        public IActionResult Categories(Category category)
+        {
+            _catalog.Categories.Add(category);
+            return View(_catalog);
+        }
+    }
+}
