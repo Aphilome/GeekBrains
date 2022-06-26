ebb0f21346ec2eff2dc948ee9547b5d563e385df
Aura Philomena <1.katrin.1@mail.ru>
2022-06-26 16:02:40 +0300

    Потокобезопасная конструкция

--- Mvc/Catalog/Catalog/Models/Category.cs
+++ Mvc/Catalog/Catalog/Models/Category.cs
@@ -2,10 +2,32 @@
 {
     public class Category
     {
+        private List<Product> _products { get; set; } = new();
+        private object _writeLock = new object();
+
+        public Category() { }
+
+        public Category(int id, string name) 
+        {
+            Id = id;
+            Name = name;
+        }
+
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
-        public List<Product> Products { get; set; } = new();
+        public void AddNewProduct(Product product)
+        {
+            lock(_writeLock)
+            {
+                _products.Add(product);
+            }
+        }
+
+        public IReadOnlyCollection<Product> GetProducts()
+        {
+            return _products;
+        }
     }
 }
