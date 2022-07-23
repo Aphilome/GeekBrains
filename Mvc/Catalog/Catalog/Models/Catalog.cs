4af73a4eef7a8e2aa29eb04224526a5010d2f860
Aura Philomena <1.katrin.1@mail.ru>
2022-07-23 15:14:44 +0300

    добавление метод удаления

--- Mvc/Catalog/Catalog/Models/Catalog.cs
+++ Mvc/Catalog/Catalog/Models/Catalog.cs
@@ -4,33 +4,36 @@ namespace Catalog.Models
 {
     public class Catalog
     {
-        private ConcurrentBag<Category> _categories { get; }
+        private ConcurrentDictionary<int, Category> _categories { get; }
 
         public Catalog()
         {
-            var vegetablesCategory = new Models.Category(1, "Vegetables");
+            var vegetablesCategory = new Category(1, "Vegetables");
             vegetablesCategory.AddNewProduct(new Product(1, vegetablesCategory.Id, "Potetor"));
             vegetablesCategory.AddNewProduct(new Product(2, vegetablesCategory.Id, "Tomato"));
             
-            var fruitsCategory = new Models.Category(2, "Fruits");
+            var fruitsCategory = new Category(2, "Fruits");
             fruitsCategory.AddNewProduct(new Product(1, fruitsCategory.Id, "Apple"));
             fruitsCategory.AddNewProduct(new Product(2, fruitsCategory.Id, "Orange"));
 
-            _categories = new ConcurrentBag<Category>
-            {
-                vegetablesCategory,
-                fruitsCategory
-            };
+            _categories = new ConcurrentDictionary<int, Category>();
+            _categories[vegetablesCategory.Id] = vegetablesCategory;
+            _categories[fruitsCategory.Id] = fruitsCategory;
         }
 
         public void AddCategory(Category newCategory)
         {
-            _categories.Add(newCategory);
+            _categories[newCategory.Id] = newCategory;
         }
 
         public IReadOnlyCollection<Category> GetCategories()
         {
-            return _categories;
+            return _categories.Values.ToArray();
+        }
+
+        public void RemoveCategory(Category removeCategory)
+        {
+            _categories.Remove(removeCategory.Id, out _);
         }
     }
 }
