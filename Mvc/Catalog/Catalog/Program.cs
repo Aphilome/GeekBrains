dafdb9b0bb93a417b931bd1ee1e559d7f066bfd2
Aura Philomena <1.katrin.1@mail.ru>
2022-08-01 02:29:35 +0300

    serilog

--- Mvc/Catalog/Catalog/Program.cs
+++ Mvc/Catalog/Catalog/Program.cs
@@ -2,37 +2,63 @@ using Catalog.Configs;
 using Catalog.Services.Abstract;
 using Catalog.Services.Concrete;
 using MailKit.Net.Smtp;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up!");
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.Configure<SmtpCredentials>(builder.Configuration.GetSection("SmtpCredentials"));
-
-builder.Services.AddScoped<SmtpClient>();
-builder.Services.AddScoped<IMailSender, SmtpMailSender>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((_, conf) => conf.WriteTo.Console());
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.Configure<SmtpCredentials>(builder.Configuration.GetSection("SmtpCredentials"));
+
+    builder.Services.AddScoped<SmtpClient>();
+    builder.Services.AddScoped<IMailSender, SmtpMailSender>();
+    builder.Services.AddScoped<IProductService, ProductService>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    // TODO: Remove it in .NET 7
+    if (ex.GetType().Name is "StopTheHostException")
+    {
+        throw;
+    }
+    Log.Fatal(ex, "HUSTON!!!");
+    throw;
+}
+finally
+{
+    Log.Information("Good buy");
+    Log.CloseAndFlush();
+}
