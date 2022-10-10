5dcea7745a21db1fe76653434642ba525ad9cc77
Nikolay Ivanov <nikolay9619962@mail.ru>
2022-10-10 07:54:03 +0300

    empty

--- Security/CardStorageService/Account.Helper/Program.cs
+++ Security/CardStorageService/Account.Helper/Program.cs
@@ -4,6 +4,7 @@ namespace Account.Helper
 {
     internal class Program
     {
+        // empty PR for 5 lesson
         static void Main(string[] args)
         {
             var res = PasswordUtils.CreatePasswordHash("123");
