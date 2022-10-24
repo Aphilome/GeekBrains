aade5fb11810b5849a61150fe8bc487f159932b5
Ekaterina Ivanova <1.katrin.1@mail.ru>
2022-10-24 15:28:53 +0300

    async locking

--- Message/MdaRestaurant/MdaRestaurant/Models/Restaurant.cs
+++ Message/MdaRestaurant/MdaRestaurant/Models/Restaurant.cs
@@ -5,9 +5,9 @@ namespace MdaRestaurant.Models;
 internal class Restaurant
 {
     private readonly List<Table> _tables = new();
-    private object _lock = new();
+    private readonly SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
 
-	public Restaurant()
+    public Restaurant()
 	{
 		for (ushort i = 1; i <= 10; i++)
 			_tables.Add(new Table(i));
@@ -21,10 +21,18 @@ internal class Restaurant
     {
         Console.WriteLine("Hi! I will check the table and approve booking, please be in line");
 
-        var table = _tables.FirstOrDefault(i => i.SeatsCount > countOfPersons && i.State == Enums.State.Free);
-
-        Thread.Sleep(5000); // so slow managers
-        table?.SetState(State.Booked);
+        Table? table;
+        _lock.Wait();
+        try
+        { 
+            table = _tables.FirstOrDefault(i => i.SeatsCount > countOfPersons && i.State == Enums.State.Free);
+            table?.SetState(State.Booked);
+            Thread.Sleep(5000); // so slow managers
+        }
+        finally
+        {
+            _lock.Release();
+        }
 
         Console.WriteLine(table is null
             ? "Sorry, we havn't free tables"
@@ -41,9 +49,18 @@ internal class Restaurant
 
         Task.Run(async () =>
         {
-            var table = _tables.FirstOrDefault(i => i.SeatsCount > countOfPersons && i.State == Enums.State.Free);
-            await Task.Delay(5000); // so slow managers
-            table?.SetState(State.Booked);
+            Table? table;
+            await _lock.WaitAsync();
+            try
+            {
+                table = _tables.FirstOrDefault(i => i.SeatsCount > countOfPersons && i.State == Enums.State.Free);
+                table?.SetState(State.Booked);
+                await Task.Delay(5000); // so slow managers
+            }
+            finally
+            {
+                _lock.Release();
+            }
 
             Console.WriteLine(table is null
                 ? "NOTIFICATION: Sorry, we havn't free tables"
