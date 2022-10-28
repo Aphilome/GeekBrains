df83ec613b0e3a8412dc569993624211f9d8ba9a
Ekaterina Ivanova <1.katrin.1@mail.ru>
2022-10-28 22:03:45 +0300

    save url

--- Message/MdaRestaurant/Messaging/RabbitMqConnectionSettings.cs
+++ Message/MdaRestaurant/Messaging/RabbitMqConnectionSettings.cs
@@ -1,5 +1,9 @@
 ﻿namespace Messaging;
 
+/*
+https://api.cloudamqp.com/console/10ead49b-969e-47b7-af82-b59222f501e5/details
+*/
+
 internal static class RabbitMqConnectionSettings
 {
     public static string HostName => "goose.rmq2.cloudamqp.com";
