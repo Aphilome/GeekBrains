dbc687e093c12f81b2f8cda215af57a366a080fe
Ekaterina Ivanova <1.katrin.1@mail.ru>
2022-11-01 22:36:38 +0300

    Basic work

---
+++ Message/MdaRestaurant/Messaging/InMemoryDb/IInMemoryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Restaurant.Messages.InMemoryDb;
+
+public interface IInMemoryRepository<T> where T : class
+{
+    public void AddOrUpdate(T entity);
+
+    public IEnumerable<T> Get();
+}
