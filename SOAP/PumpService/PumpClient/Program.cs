0bace33d678454eb6e8ff7e18d16c425ff8f3e88
Ekaterina Ivanova <1.katrin.1@mail.ru>
2022-11-07 23:37:25 +0300

    WCF service

---
+++ SOAP/PumpService/PumpClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using PumpClient.PumpServiceReference;
+using System;
+using System.ServiceModel;
+
+namespace PumpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var instanceContext = new InstanceContext(new CallbackHandler());
+            var client = new PumpnServiceClient(instanceContext);
+
+            client.UpdateAndCompileScript(@"C:\scripts\Sample.script");
+            client.RunScript();
+
+            Console.WriteLine("Please, Enter to exit ...");
+            Console.ReadKey(true);
+            client.Close();
+        }
+    }
+}
